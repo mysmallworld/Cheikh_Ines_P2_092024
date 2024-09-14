840611a8c7bbd8e9434d5dc45db8cc055d4f84b3
Inès C <ines.parisdescartes@gmail.com>
2024-09-14 22:13:45 +0200

    feat(dashboard): add graphic with data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -10,9 +11,54 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
 
+  // options
+  view: [number, number] = [800, 400];
+  showLegend: boolean = false;
+  showLabels: boolean = true;
+  showAnimation: boolean = false;
+  isExplodeSlices: boolean = false;
+  isDoughnut: boolean = false;
+  isGradient: boolean = false;
+
+  // colors
+  public colorScheme: any = {
+    domain: ['#A66870', '#8B415D', '#90ACE2', '#A48AAC', '#C1E5F4','#BDD2EB' ]
+  };
+
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
+    // data
     this.olympics$ = this.olympicService.getOlympics();
+  }
+
+  /**
+   * Method to get number of countries
+   * @param olympics data of country in Olympics
+   * @returns number of countries
+   */
+  getNumberOfCountries(olympics: Olympic[]): number{
+    return olympics.length;
+  }
+
+  /**
+   * Method to get number of JOs
+   * @param olympics data of country in Olympics
+   * @returns number of JOs
+   */
+  getNumberOfJOs(olympics: Olympic[]): number {
+    return olympics.map(olympic => olympic.participations)[0].length;
+  }
+
+  /**
+   * Method to get medals per country
+   * @param olympics data of country in Olympics
+   * @returns total medals count per country
+   */
+  getMedalsPerCountry(olympics: Olympic[]): { name: string; value: number; }[] {
+    return olympics.map(olympic => ({
+      name: olympic.country,
+      value: olympic.participations.reduce((total, participation) => total + participation.medalsCount, 0)
+    }));
   }
 }
