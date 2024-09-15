6823059b0d36e2efdc3c1cc4f66687583feeddd4
Inès C <ines.parisdescartes@gmail.com>
2024-09-15 21:23:10 +0200

    feat(detail): add route between pages

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { Router } from '@angular/router';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+
+  public selectedCountryData: Olympic | null = null;
 
   // options
   view: [number, number] = [800, 400];
@@ -22,13 +25,16 @@ export class HomeComponent implements OnInit {
 
   // colors
   public colorScheme: any = {
-    domain: ['#A66870', '#8B415D', '#90ACE2', '#A48AAC', '#C1E5F4', '#BDD2EB' ]
+    domain: ['#A66870', '#8B415D', '#90ACE2', '#A48AAC', '#C1E5F4', '#BDD2EB'],
   };
 
-  constructor(private olympicService: OlympicService) {}
+  constructor(
+    private olympicService: OlympicService, 
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    // data
+    // Olympics data
     this.olympics$ = this.olympicService.getOlympics();
   }
 
@@ -55,10 +61,31 @@ export class HomeComponent implements OnInit {
    * @param olympics data of country in Olympics
    * @returns total medals count per country
    */
-  getMedalsPerCountry(olympics: Olympic[]): { name: string; value: number; }[] {
-    return olympics.map(olympic => ({
+  getMedalsPerCountry(olympics: Olympic[]): { name: string; value: number }[] {
+    return olympics.map((olympic) => ({
       name: olympic.country,
-      value: olympic.participations.reduce((total, participation) => total + participation.medalsCount, 0)
+      value: olympic.participations.reduce(
+        (total, participation) => total + participation.medalsCount,
+        0
+      ),
     }));
+  }
+
+  /**
+   * Method to get data of selected country
+   * @param event 
+   */
+  getOlympicsByCountry(event: any): void {
+    const selectedCountry = event.name;
+
+    this.olympics$.subscribe((olympics) => {
+      this.selectedCountryData = olympics.find(
+        (olympic: { country: any }) => olympic.country === selectedCountry
+      );
+    });
+
+    if (this.selectedCountryData) {
+      this.router.navigate(['/detail'], { queryParams: { country: selectedCountry } });
+    }
   }
 }
